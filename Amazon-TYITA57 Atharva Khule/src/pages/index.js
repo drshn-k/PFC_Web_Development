421d9177832fafe752ef165f069e673c7be550e9
Atharva Khule <85023327+AtharvaKhule-12@users.noreply.github.com>
2023-04-13 12:53:16 +0530

    Add files via upload
    Atharva Khule, Utkarsh Bhole, Manas Mahajan

---
+++ Amazon-TYITA57 Atharva Khule/src/pages/index.js
@@ -0,0 +1,40 @@
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({ products }) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+
+        {/* ProdcutFeed */}
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  );
+}
+
+//this function is basically NodeJS
+export const getServerSideProps = async (context) => {
+  const products = await fetch('https://fakestoreapi.com/products').then(res => res.json());
+  const session = await getSession(context);
+
+  return {
+    props: {
+      products,
+      session
+    },
+  };
+
+}
