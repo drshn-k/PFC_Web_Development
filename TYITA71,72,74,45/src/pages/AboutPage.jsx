73c6c8d3fb3a1fc5776a20d4c51825dfbb2303b8
Yash Tembhurnikar <85123439+Yash-11-star@users.noreply.github.com>
2023-04-13 13:17:34 +0530

    TYITA71,72,74,45

---
+++ TYITA71,72,74,45/src/pages/AboutPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
+          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
+          totam vero quas provident ipsam, veritatis nostrum velit quos
+          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
+          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
+          porro tempore quis eaque labore voluptatibus ea necessitatibus
+          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
+          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
+          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
+          delectus expedita a alias nam recusandae illo debitis repellat libero,
+          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
+          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://media.gettyimages.com/id/831659416/photo/luxury-clothing-shop-for-men.jpg?s=1024x1024&w=gi&k=20&c=YniAi2r7Qk-7cEQosBiH4LRpa-09t6xOMLsNUixAlDw=" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.unsplash.com/photo-1601121141461-9d6647bca1ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1437&q=80" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://thumbs.dreamstime.com/z/many-used-modern-electronic-gadgets-use-white-floor-reuse-recycle-concept-top-view-153892483.jpg" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
